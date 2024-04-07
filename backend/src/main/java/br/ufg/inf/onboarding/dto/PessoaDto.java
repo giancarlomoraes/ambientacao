2a5f9863a396239429d5d9e53768d671282adbe2
Giancarlo Moraes <gmoraes@labtime.ufg.br>
2024-04-07 09:45:04 -0300

    feat (edicao-usuario): Adiciona dependência para validação dos DTO's

--- backend/src/main/java/br/ufg/inf/onboarding/dto/PessoaDto.java
+++ backend/src/main/java/br/ufg/inf/onboarding/dto/PessoaDto.java
@@ -2,12 +2,18 @@ package br.ufg.inf.onboarding.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class PessoaDto {
+
+
     private Integer id;
 
+    @NotBlank(message = "Nome não pode estar em branco")
     private String nome;
 
+    @NotBlank(message = "CPF não pode estar em branco")
     private String cpf;
 
 }
