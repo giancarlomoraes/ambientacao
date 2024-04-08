508807e94143b9ea4e0db1d0bed1a5b95a881ed6
Giancarlo Moraes <gmoraes@labtime.ufg.br>
2024-04-08 18:29:39 -0300

    feat (testes-usuario): Adiciona teste que valida erro ao tentar cadastrar um usuário com CPF já cadastrado no banco

--- backend/src/test/java/br/ufg/inf/onboarding/service/PessoaServiceTest.java
+++ backend/src/test/java/br/ufg/inf/onboarding/service/PessoaServiceTest.java
@@ -1,5 +1,6 @@
 package br.ufg.inf.onboarding.service;
 
+import br.ufg.inf.onboarding.exception.CustomException;
 import br.ufg.inf.onboarding.model.Pessoa;
 import br.ufg.inf.onboarding.repository.PessoaRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,6 +37,29 @@ public class PessoaServiceTest {
         assertNotNull(savedPessoa);
         assertEquals("Teste Nome", savedPessoa.getNome());
         verify(pessoaRepository).save(any(Pessoa.class));
+    }
+
+    @Test
+    void criarPessoaComCpfDuplicadoDeveLancarCustomException() {
+        // Configura o repository para simular um CPF já existente
+        Pessoa pessoaExistente = new Pessoa();
+        pessoaExistente.setNome("Uma pessoa");
+        pessoaExistente.setCpf("025.592.689-81");
+        when(pessoaRepository.findPessoaByCpfEquals(anyString())).thenReturn(pessoaExistente);
+
+        // Cria uma nova pessoa com o mesmo CPF para testar a criação
+        Pessoa novaPessoa = new Pessoa();
+        novaPessoa.setNome("Teste");
+        novaPessoa.setCpf("025.592.689-81"); // O mesmo CPF, formatado diferente
+
+        // Verifica se a CustomException é lançada com a mensagem esperada
+        CustomException thrown = assertThrows(
+                CustomException.class,
+                () -> pessoaService.criarPessoa(novaPessoa),
+                "Esperado CustomException ao tentar criar uma pessoa com CPF duplicado"
+        );
+
+        assertEquals("CPF já cadastrado", thrown.getDescricao());
     }
 }
 
